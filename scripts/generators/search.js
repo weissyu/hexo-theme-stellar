bba63df6ca9ea85e38d83537c4bdd33884e0dc5b
weissyu <weissyu@outlook.com>
2022-12-17 16:34:29 +0800

    fix: search.js 調整語法

--- scripts/generators/search.js
+++ scripts/generators/search.js
@@ -32,43 +32,40 @@ hexo.extend.generator.register('search_json_generator', function (locals) {
     if (cfg.content != false && post._content) {
       var content = post._content.trim()
       // 过滤掉标签和注释
-      if (!content.includes('{%')) {
+      if (content.includes('{%')) {
         // 需要保留内容的的标签
         content = content.replace(/{%\s*mark\s*(.*?)\s*%}/g, '$1')
-        content = content.replace(/{%\s*folding\s*(.*?)\s*%}/g, '$1')
-        content = content.replace(/{%\s*copy\s*(.*?)\s*%}/g, '$1')
-        content = content.replace(/{%\s*note\s*(.*?)\s*%}/g, '$1')
-        content = content.replace(/{%\s*kbd\s*(.*?)\s*%}/g, '$1')
-        content = content.replace(/{%\s*emp\s*(.*?)\s*%}/g, '$1')
-        content = content.replace(/{%\s*wavy\s*(.*?)\s*%}/g, '$1')
-        content = content.replace(/{%\s*sub\s*(.*?)\s*%}/g, '$1')
-        content = content.replace(/{%\s*sup\s*(.*?)\s*%}/g, '$1')
-        content = content.replace(/<!--\s*folder(.*?)\s*-->/g, '$1')
-        content = content.replace(/<!--\s*tab(.*?)\s*-->/g, '$1')
-        content = content.replace(/<!--\s*node(.*?)\s*-->/g, '$1')
-        // 不保留内容的标签
-        content = content.replace(/{%\s*(.*?)\s*%}/g, '')
-        // 注释
-        content = content.replace(/<!--\s*(.*?)\s*-->/g, '')
-        // ## 标题
-        content = content.replace(/[#]{2,} /g, '')
-        // 部分HTML标签
-        content = content.replace(/<iframe[\s|\S]+iframe>/g, '')
-        content = content.replace(/<hr>/g, '')
-        content = content.replace(/<br>/g, '')
-        // 图片
-        content = content.replace(/\!\[(.*?)\]\((.*?)\)/g, '')
-        // 链接
-        content = content.replace(/\[(.*?)\]\((.*?)\)/g, '$1')
-        // 过滤代码块
-        if (cfg.codeblock == false) {
-          content = content.replace(/```([^`]+)```/g, '')
-        }
-        // 多个连续空格换成单个空格
-        content = content.replace(/[\s]{2,} /g, ' ')
-        // 特殊字符
-        content = content.replace(/[\r|\n]+/g, '')
+          .replace(/{%\s*folding\s*(.*?)\s*%}/g, '$1')
+          .replace(/{%\s*copy\s*(.*?)\s*%}/g, '$1')
+          .replace(/{%\s*note\s*(.*?)\s*%}/g, '$1')
+          .replace(/{%\s*kbd\s*(.*?)\s*%}/g, '$1')
+          .replace(/{%\s*emp\s*(.*?)\s*%}/g, '$1')
+          .replace(/{%\s*wavy\s*(.*?)\s*%}/g, '$1')
+          .replace(/{%\s*sub\s*(.*?)\s*%}/g, '$1')
+          .replace(/{%\s*sup\s*(.*?)\s*%}/g, '$1')          
+          .replace(/{%\s*(.*?)\s*%}/g, '') // 不保留内容的标签
       }
+
+      if (content.includes('<')) {
+        content = content.replace(/<!--\s*tab(.*?)\s*-->/g, '$1')
+          .replace(/<!--\s*node(.*?)\s*-->/g, '$1')
+          .replace(/<!--\s*folder(.*?)\s*-->/g, '$1')
+          .replace(/<!--\s*(.*?)\s*-->/g, '')// 注释       
+          .replace(/<iframe[\s|\S]+iframe>/g, '')// 部分HTML标签
+          .replace(/<hr>/g, '')
+          .replace(/<br>/g, '')
+      }
+      
+      content = content.replace(/[#]{2,} /g, '') // ## 标题      
+        .replace(/\!\[(.*?)\]\((.*?)\)/g, '') // 图片
+        .replace(/\[(.*?)\]\((.*?)\)/g, '$1') // 链接      
+        .replace(/[\s]{2,} /g, ' ')// 多个连续空格换成单个空格
+        .replace(/[\r|\n]+/g, '')// 特殊字符      
+      
+      if (cfg.codeblock == false) {
+        content = content.replace(/```([^`]+)```/g, '')// 过滤代码块
+      }      
+      
       temp_post.content = content.trim()
     }
     if (post.tags && post.tags.length > 0) {
@@ -89,14 +86,14 @@ hexo.extend.generator.register('search_json_generator', function (locals) {
   }
 
   if (posts) {
-    posts.each(function(post) {
+    posts.each(function (post) {
       if (post.indexing == false) return
       let temp_post = generateJson(post)
       res.push(temp_post)
-    }) 
-  } 
+    })
+  }
   if (pages) {
-    pages.each(function(page) {
+    pages.each(function (page) {
       if (page.indexing == false) return
       let temp_post = generateJson(page)
       res.push(temp_post)
